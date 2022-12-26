c08fc35dbd3961e972faa6eec8a9a44e02e4fdac
SofiaChyrun <chyrunsofia@gmail.com>
2022-12-26 23:39:38 +0200

    master commit

---
+++ UnitTest11.4/UnitTest11.4.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ChyrunSofialab11.4/ChyrunSofialab11.4.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest114
+{
+	TEST_CLASS(UnitTest114)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			int N = 3, s = 0;
+			Price* p = new Price[N];
+			p[0].cost = 9;
+			p[1].cost = 27;
+			p[2].cost = 99;
+			t = (int)MinSerbal(p, N);
+			Assert::AreEqual(t, 9);
+		}
+	};
+}
